649a4dd501232f29982a73bebcd745c01f7e55d0
Sergio Oller <sergioller@gmail.com>
2019-11-21 08:06:46 +0100

    try fix appveyor

--- tests/testthat/test-label-number-si.R
+++ tests/testthat/test-label-number-si.R
@@ -1,7 +1,9 @@
 test_that("rescales values independently", {
   number_si <- label_number_si()
   expect_equal(
-    number_si(10^c(-24, -21, -18, -15, -12, -9, -6, -3, 0, 3, 6, 9, 12, 15, 18, 21, 24)),
+    enc2utf8(
+      number_si(10^c(-24, -21, -18, -15, -12, -9, -6, -3, 0, 3, 6, 9, 12, 15, 18, 21, 24))
+    ),
     c("1y", "1z", "1a", "1f", "1p", "1n", "1\u03bc", "1m", "1", "1k", "1M", "1G", "1T", "1P", "1E", "1Z", "1Y")
   )
   expect_equal(number_si(c(-1e3, 1e6, 1e9)), c("-1k", "1M", "1G"))
